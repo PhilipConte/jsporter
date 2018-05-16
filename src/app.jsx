90ff9230cc2864ba2798bc125875604b04f10493
Philip Conte <philipmconte@gmail.com>
2018-05-16 16:28:29 -0400

    added react-tabs

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 
 export default class App extends React.Component {
   render() {
     return (<div>
-      <h2>Welcome to React!</h2>
+      <Tabs>
+        <TabList>
+          <Tab>Title 1</Tab>
+          <Tab>Title 2</Tab>
+        </TabList>
+
+        <TabPanel>
+          <h2>Welcome to React!</h2>
+        </TabPanel>
+        <TabPanel>
+          <h2>Hello World!</h2>
+        </TabPanel>
+      </Tabs>
     </div>);
   }
 }
