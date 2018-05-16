152ea49b705870c38753da71374ae8de8f04a90d
Philip Conte <philipmconte@gmail.com>
2018-05-16 18:07:22 -0400

    switched spacing from 2 to 4

--- src/app.jsx
+++ src/app.jsx
@@ -3,28 +3,28 @@ import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import SplitPlane from 'react-split-pane';
 
 export default class App extends React.Component {
-  render() {
-    return (<div>
-      <Tabs>
-        <TabList>
-          <Tab>Title 1</Tab>
-          <Tab>Title 2</Tab>
-        </TabList>
+    render() {
+        return (<div>
+            <Tabs>
+                <TabList>
+                    <Tab>Title 1</Tab>
+                    <Tab>Title 2</Tab>
+                </TabList>
 
-        <TabPanel>
-          <SplitPlane defaultSize={200}>
-            <div>
-              <h2>Hello World!</h2>
-            </div>
-            <div>
-              <h2>Welcome to React!</h2>
-            </div>
-          </SplitPlane>
-        </TabPanel>
-        <TabPanel>
-          <h2>just here for show</h2>
-        </TabPanel>
-      </Tabs>
-    </div>);
-  }
+                <TabPanel>
+                    <SplitPlane defaultSize={200}>
+                        <div>
+                            <h2>Hello World!</h2>
+                        </div>
+                        <div>
+                            <h2>Welcome to React!</h2>
+                        </div>
+                    </SplitPlane>
+                </TabPanel>
+                <TabPanel>
+                    <h2>just here for show</h2>
+                </TabPanel>
+            </Tabs>
+        </div>);
+    }
 }
