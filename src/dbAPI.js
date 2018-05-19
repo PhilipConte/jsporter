a3006158922e4f20607de8e61b7cadf9e85590b1
Philip Conte <philipmconte@gmail.com>
2018-05-19 06:10:22 -0400

    changed ES6 style exports

--- src/dbAPI.js
+++ src/dbAPI.js
@@ -1,25 +1,22 @@
-module.exports = {
-    openDB: function(dbPath) {
-        const sqlite3 = require('sqlite3').verbose();
-        return new sqlite3.Database(dbPath);
-    },
-
-    dbfoo: function(db)
-    {
-        db.serialize(function() {
-            db.run("CREATE TABLE if not exists lorem (info TEXT)");
-
-            var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
-            for (var i = 0; i < 10; i++) {
-                stmt.run("Ipsum " + i);
-            }
-            stmt.finalize();
-
-            db.each("SELECT rowid AS id, info FROM lorem", function(err, row) {
-                console.log(row.id + ": " + row.info);
-            });
-        });
-    },
-
-    dbClose: function(db) { db.close(); }
+export function openDB(dbPath) {
+    const sqlite3 = require('sqlite3').verbose();
+    return new sqlite3.Database(dbPath);
 }
+
+export function dbfoo(db) {
+    db.serialize(function() {
+        db.run("CREATE TABLE if not exists lorem (info TEXT)");
+
+        var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
+        for (var i = 0; i < 10; i++) {
+            stmt.run("Ipsum " + i);
+        }
+        stmt.finalize();
+
+        db.each("SELECT rowid AS id, info FROM lorem", function(err, row) {
+            console.log(row.id + ": " + row.info);
+        });
+    });
+}
+
+export function dbClose(db) { db.close(); }
