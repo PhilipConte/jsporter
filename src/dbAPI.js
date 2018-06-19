ef24ef491884d14fc569b298f1afef0985e7e097
PhilipConte <philipmconte@gmail.com>
2018-06-19 10:15:44 -0400

    removed cruft comments

--- src/dbAPI.js
+++ src/dbAPI.js
@@ -1,28 +1,7 @@
 import Sequelize from 'sequelize';
 import {pathToName} from './util';
 import {ezError} from './dialog';
-/*export function openDB(dbPath) {
-    const sqlite3 = require('sqlite3').verbose();
-    return new sqlite3.Database(dbPath);
-}
 
-export function dbfoo(db) {
-    db.serialize(function() {
-        db.run("CREATE TABLE if not exists lorem (info TEXT)");
-
-        var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
-        for (var i = 0; i < 10; i++) {
-            stmt.run("Ipsum " + i);
-        }
-        stmt.finalize();
-
-        db.each("SELECT rowid AS id, info FROM lorem", function(err, row) {
-            console.log(row.id + ": " + row.info);
-        });
-    });
-}
-
-export function dbClose(db) { db.close(); }*/
 export default class DBAPI {
     constructor(p) {
         this.fp = p;
