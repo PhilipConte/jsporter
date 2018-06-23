117fa076fa6bd36ff4db3bae56f007a905156c3f
PhilipConte <philipmconte@gmail.com>
2018-06-23 18:48:34 -0400

    made inputForm "stateless"

--- src/inputForm.jsx
+++ src/inputForm.jsx
@@ -3,33 +3,29 @@ import React from 'react';
 export default class InputForm extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { value: '' };
 
-        this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
-    }
-
-    handleChange(event) {
-        this.setState({ value: event.target.value });
     }
 
     handleSubmit(event) {
         event.preventDefault();
-        this.props.submitter(this.state.value);
-        this.setState({ value: '' });
+        this.props.submitter(this.formRef.childNodes[0].childNodes[3].value);
+        this.formRef.reset();
     }
 
     render() {
         let css = this.props.className;
         return (
-            <form className={css} onSubmit={this.handleSubmit}>
+            <form className={css}
+                onSubmit={this.handleSubmit} ref={(el) => this.formRef = el}
+            >
                 <label className={css}>
                     {this.props.text}
                     <input type="text" size="10" className={css}
-                        value={this.state.value} onChange={this.handleChange}
+                        defaultValue=""
                     />
                 </label>
-                <input className={css} type="submit" value="Submit" />
+                <input className={css} type="submit" defaultValue="Submit" />
             </form>
         );
     }
