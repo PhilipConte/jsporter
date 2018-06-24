5114632309979c1dba5400fe6f598a40d30cc4f0
PhilipConte <philipmconte@gmail.com>
2018-06-24 12:39:42 -0400

    re-introduced state to inputForm

--- src/inputForm.jsx
+++ src/inputForm.jsx
@@ -3,26 +3,30 @@ import React from 'react';
 export default class InputForm extends React.Component {
     constructor(props) {
         super(props);
+        this.state = { value: '' };
 
+        this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({ value: event.target.value });
     }
 
     handleSubmit(event) {
         event.preventDefault();
-        this.props.submitter(this.formRef.childNodes[0].childNodes[3].value);
-        this.formRef.reset();
+        this.props.submitter(this.state.value);
+        this.setState({ value: '' });
     }
 
     render() {
         let css = this.props.className;
         return (
-            <form className={css}
-                onSubmit={this.handleSubmit} ref={(el) => this.formRef = el}
-            >
+            <form className={css} onSubmit={this.handleSubmit}>
                 <label className={css}>
                     {this.props.text}
                     <input type="text" size="10" className={css}
-                        defaultValue=""
+                        value={this.state.value} onChange={this.handleChange}
                     />
                 </label>
                 <input className={css} type="submit" defaultValue="Submit" />
