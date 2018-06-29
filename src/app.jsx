3c28584437f1a4287ccbd5e59aa202885d24483f
PhilipConte <philipmconte@gmail.com>
2018-06-29 08:48:16 -0400

    improved formating and moved and refactored function to util.

--- src/app.jsx
+++ src/app.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import { remote } from 'electron';
 import Store from 'electron-store';
+import CenterText from './centerText';
 import TabPanel from './tabPanel';
 import TabContent from './tabContent';
 import { getDBDialog } from './dialog';
-import {pathToName} from './util';
+import { pathToName, isKeyInStore } from './util';
 
 const store = new Store({
     name: 'renderer-preferences',
-    defaults: { 'focused': 0 } });
+    defaults: { 'focused': 0 }
+});
 
 export default class App extends React.Component {
     constructor(props) {
@@ -17,41 +19,36 @@ export default class App extends React.Component {
     }
 
     render() {
-        if (!this.state.isLoaded) {
-            return <h2>loading...</h2>
-        }
-        
+        const { isLoaded } = this.state;
+        if (!isLoaded) return <CenterText text='loading...' />;
+
         const { files } = this.state;
         const names = pathToName(files);
         const tabs = files.map(f =>
-            <TabContent path={f}/>
+            <TabContent key={f} path={f} />
         );
 
-        return (
-            <TabPanel
-                defaultIndex={this.state.focused}
-                titles={names}
-                tabs={tabs}
-            />
-        );
+        return (<TabPanel
+            defaultIndex={this.state.focused}
+            titles={names}
+            tabs={tabs}
+        />);
     }
 
     componentDidMount() {
-        function isEmpty() {
-            return (
-                !store.has('files')
-                || !store.get('files').length
-                || (
-                    store.get('files').length == 1
-                    && store.get('files')[0] == null
-                )
-            );
-        }
-        if(isEmpty()) {
+        if (!isKeyInStore(store, 'files')) {
             const tempF = getDBDialog();
-            (tempF) ? store.set('files', [tempF]): remote.getCurrentWindow().close();
-        }
-        if (store.get('focused') >= store.get('files').length) store.set('focused', 0);
-        this.setState({ isLoaded: true, focused: store.get('focused'), files: store.get('files') });
+            if (tempF) {
+                store.set('files', [tempF]);
+            } else {
+                remote.getCurrentWindow().close();
+            }
+        } if (store.get('focused') >= store.get('files').length) {
+            store.set('focused', 0);
+        } this.setState({
+            isLoaded: true,
+            focused: store.get('focused'),
+            files: store.get('files')
+        });
     }
 }
--- src/util.js
+++ src/util.js
@@ -2,3 +2,12 @@ export function pathToName(f) {
     if (Array.isArray(f)) return f.map((p) => pathToName(p));
     return f.split('\\').pop().split('/').pop().split('.').reverse()[1];
 }
+
+export const isKeyInStore = (store, key) => (
+    store.has(key)
+    && store.get(key).length
+    && (
+        store.get(key).length > 1
+        || store.get(key)[0] != null
+    )
+);
