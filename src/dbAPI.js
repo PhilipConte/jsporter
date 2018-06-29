59fabb864a04dd98aa1f2aede1c6ea6184233ef9
PhilipConte <philipmconte@gmail.com>
2018-06-29 09:28:09 -0400

    updated formatting

--- src/dbAPI.js
+++ src/dbAPI.js
@@ -1,8 +1,13 @@
 import Sequelize from 'sequelize';
-import {pathToName} from './util';
-import {ezError} from './dialog';
+import { pathToName } from './util';
+import { ezError } from './dialog';
 import Promise from 'bluebird';
 import autobind from 'autobind-decorator'
+
+const ezPromiseError = s => new Promise((r) => {
+    ezError(s);
+    throw new Error(s);
+});
 
 @autobind
 export default class DBAPI {
@@ -10,91 +15,86 @@ export default class DBAPI {
         this.fp = p;
         this.fn = pathToName(this.fp);
         this.cards = {};
-        this.cList = ()=>Object.keys(this.cards);
+        this.cList = () => Object.keys(this.cards);
     }
 
     connect() {
-        this.db = new Sequelize("sqlite:"+this.fp);
+        this.db = new Sequelize("sqlite:" + this.fp);
         return this.db.authenticate()
-        .then(()=>console.log('Connected to:', this.fp))
-        .then(()=>this.Card = this.db.define('card', { name: Sequelize.STRING }))
-        .then(()=>this.Card.sync({ force: false }))
-        .then(()=>this.Card.findAll())
-        .then((cards) => 
-            Promise.map(cards.map(c => c.get('name')).filter(c => (c && c != `''s`)),
-            c => this.createCardTable(c))
-        ).then(()=>console.log('this.cards:', this.cards))
-        .then(()=>this.db);
+            .then(() => console.log('Connected to:', this.fp))
+            .then(() => this.Card = this.db.define(
+                'card', { name: Sequelize.STRING }
+            ))
+            .then(() => this.Card.sync({ force: false }))
+            .then(() => this.Card.findAll())
+            .then((cards) =>
+                Promise.map(
+                    cards.map(c => c.get('name'))
+                        .filter(c => (c && c != `''s`)),
+                    c => this.createCardTable(c)
+                )
+            ).then(() => console.log('this.cards:', this.cards))
+            .then(() => this.db);
     }
 
     addCard(card) {
         if (!card.length) {
-            ezError("Empty Card Name!");
-            return new Promise((res)=>{throw new Error("Empty Card Name!");});
-        }
-        if (this.cList().includes(card)) {
-            ezError("Duplicate Card!");
-            return new Promise((res)=>{throw new Error("Duplicate Card!");});
-        }
-        return this.Card.sync({ force: false })
-        .then(()=>this.Card.create({ name: card }))
-        .then(()=>this.createCardTable(card))
-        .then(()=> this.cList());
+            return ezPromiseError("Empty Card Name");
+        } if (this.cList().includes(card)) {
+            return ezPromiseError("Duplicate Card");
+        } return this.Card.sync({ force: false })
+            .then(() => this.Card.create({ name: card }))
+            .then(() => this.createCardTable(card))
+            .then(() => this.cList());
     }
 
     createCardTable(card) {
         const newTable = this.db.define(card,
-            {entry: Sequelize.STRING, content: Sequelize.STRING},
-            {freezeTableName: true});
+            { entry: Sequelize.STRING, content: Sequelize.STRING },
+            { freezeTableName: true });
         return newTable.sync({ force: false })
-        .then(()=>this.cards[card] = newTable)
-        .then(()=>console.log('created table:', card));
+            .then(() => this.cards[card] = newTable)
+            .then(() => console.log('created table:', card));
     }
 
     readCard(card) {
         return this.cards[card].findAll()
-        .then(rows=>rows.map(r=>[r.get('entry'), r.get('content')]));
+            .then(rows => rows.map(r => [r.get('entry'), r.get('content')]));
     }
 
     addEntry(card, entry) {
-        if (!entry.length) {
-            ezError("Empty Entry Name!");
-            return new Promise((res)=>{throw new Error("Empty Entry Name!");});
-        }
+        if (!entry.length) return ezPromiseError("Empty Entry Name");
         return this.cards[card]
-        .findOrCreate({where: {entry: entry}, defaults: {content: ''}})
-        .then(data=> {
-            let created = data[1];
-            if (!created) {
-                ezError("Duplicate Entry!");
-                throw new Error("Duplicate Entry!");
-            }
-        }).then(()=>this.readCard(card));
+            .findOrCreate(
+                { where: { entry: entry }, defaults: { content: '' } }
+            )
+            .then(data => {
+                let created = data[1];
+                if (!created) return ezPromiseError("Duplicate Entry");
+            }).then(() => this.readCard(card));
     }
 
     updateContent(card, entry, text) {
         return this.cards[card]
-        .find({ where: { entry: entry } })
-        .then(record=>{
-            if (!record) {
-                ezError("Entry Not Found!");
-                throw new Error("Entry Not Found!")
-            } else {return record};
-        }).then(record=>record.updateAttributes({content: text})
-        .then(()=>this.readCard(card)));
+            .find({ where: { entry: entry } })
+            .then(record => {
+                if (!record) return ezPromiseError('Entry Not Found');
+                return record;
+            }).then(record => record.updateAttributes({ content: text })
+                .then(() => this.readCard(card)));
     }
 
     deleteEntry(card, entry) {
         return this.cards[card]
-        .destroy({ where: { entry: entry }, limit: 1 })
-        .then(()=>this.readCard(card));
+            .destroy({ where: { entry: entry }, limit: 1 })
+            .then(() => this.readCard(card));
     }
 
     deleteCard(card) {
         return this.Card
-        .destroy({ where: { name: card }, limit: 1 })
-        .then(()=>this.cards[card].drop())
-        .then(()=> delete this.cards[card])
-        .then(()=>this.cList());
+            .destroy({ where: { name: card }, limit: 1 })
+            .then(() => this.cards[card].drop())
+            .then(() => delete this.cards[card])
+            .then(() => this.cList());
     }
 }
--- src/tabContent.jsx
+++ src/tabContent.jsx
@@ -13,44 +13,46 @@ export default class TabContent extends React.Component {
     @autobind
     createCard(card) {
         this.state.db.addCard(card)
-        .then(cards => this.setState({cards: cards}))
-        .then(()=>this.selectCard(card));
+            .then(cards => this.setState({ cards: cards }))
+            .then(() => this.selectCard(card));
     }
 
     @autobind
     selectCard(card) {
         this.state.db.readCard(card)
-        .then(data => this.setState({selected: card, cardData: data}));
+            .then(data => this.setState({ selected: card, cardData: data }));
     }
 
     @autobind
     addEntry(card, entry) {
         this.state.db.addEntry(card, entry)
-        .then(data => this.setState({cardData: data}));
+            .then(data => this.setState({ cardData: data }));
     }
-    
+
     @autobind
     updateContent(card, entry, text) {
         this.state.db.updateContent(card, entry, text)
-        .then(data => this.setState({cardData: data}));
+            .then(data => this.setState({ cardData: data }));
     }
 
     @autobind
     deleteEntry(card, entry) {
         this.state.db.deleteEntry(card, entry)
-        .then(data => this.setState({cardData: data}));
+            .then(data => this.setState({ cardData: data }));
     }
 
     @autobind
     deleteCard(card) {
         this.state.db.deleteCard(card)
-        .then(cards=>{this.setState({selected: null, cards: cards});return cards;});
+            .then(cards => {
+                this.setState({ selected: null, cards: cards });
+                return cards;
+            });
     }
 
     render() {
-        if (!this.state.isLoaded) {
-            return  <CenterText text='loading...'/>;
-        }
+        if (!this.state.isLoaded) return <CenterText text='loading...' />;
+        
         return (<CardView
             card={this.state.selected}
             cards={this.state.cards}
@@ -67,8 +69,8 @@ export default class TabContent extends React.Component {
     componentDidMount() {
         var db = new DBAPI(this.props.path);
         db.connect()
-        .then(()=> 
-            this.setState({ isLoaded: true, db: db, cards: db.cList() })
-        );
+            .then(() =>
+                this.setState({ isLoaded: true, db: db, cards: db.cList() })
+            );
     }
 }
