638c4c3b63cec1c1cfdd526e83d5e6e3581368e5
PhilipConte <Philipmconte@gmail.com>
2018-10-20 15:31:39 -0400

    refactored dbAPI to use async/await

--- src/dbAPI.js
+++ src/dbAPI.js
@@ -18,86 +18,81 @@ export default class DBAPI {
         this.cList = () => Object.keys(this.cards);
     }
 
-    connect() {
+    async connect() {
         this.db = new Sequelize('', '', '', {
             dialect: 'sqlite',
             storage: this.fp
         });
-        return this.db.authenticate()
-            .then(() => console.log('Connected to:', this.fp))
-            .then(() => this.Card = this.db.define(
-                'card', { name: Sequelize.STRING }
-            ))
-            .then(() => this.Card.sync({ force: false }))
-            .then(() => this.Card.findAll())
-            .then((cards) =>
-                Promise.map(
-                    cards.map(c => c.get('name'))
-                        .filter(c => (c && c != `''s`)),
-                    c => this.createCardTable(c)
-                )
-            ).then(() => console.log('this.cards:', this.cards))
-            .then(() => this.db);
+        await this.db.authenticate()
+        console.log('Connected to:', this.fp);
+        this.Card = await this.db.define(
+            'card', { name: Sequelize.STRING }
+        );
+        await this.Card.sync({ force: false });
+        const cards = await this.Card.findAll();
+        await Promise.map(
+            cards.map(c => c.get('name'))
+                .filter(c => (c && c != `''s`)),
+            c => this.createCardTable(c)
+        );
+        console.log('this.cards:', this.cards);
+        return this.db;
     }
 
-    createCard(card) {
+    async createCard(card) {
         if (!card.length) {
             return ezPromiseError("Empty Card Name");
         } if (this.cList().includes(card)) {
             return ezPromiseError("Duplicate Card");
-        } return this.Card.sync({ force: false })
-            .then(() => this.Card.create({ name: card }))
-            .then(() => this.createCardTable(card))
-            .then(() => this.cList());
+        }
+        await this.Card.sync({ force: false });
+        await this.Card.create({ name: card });
+        await this.createCardTable(card);
+        return this.cList();
     }
 
-    createCardTable(card) {
-        const newTable = this.db.define(card,
+    async createCardTable(card) {
+        const newTable = this.db.define(
+            card,
             { entry: Sequelize.STRING, content: Sequelize.STRING },
-            { freezeTableName: true });
-        return newTable.sync({ force: false })
-            .then(() => this.cards[card] = newTable)
-            .then(() => console.log('created table:', card));
+            { freezeTableName: true }
+        );
+        await newTable.sync({ force: false });
+        this.cards[card] = newTable;
+        console.log('created table:', card);
     }
 
-    createEntry(card, entry) {
+    async createEntry(card, entry) {
         if (!entry.length) return ezPromiseError("Empty Entry Name");
-        return this.cards[card]
-            .findOrCreate(
-                { where: { entry: entry }, defaults: { content: '' } }
-            )
-            .then(data => {
-                let created = data[1];
-                if (!created) return ezPromiseError("Duplicate Entry");
-            }).then(() => this.readCard(card));
+        const data = await this.cards[card].findOrCreate(
+            { where: { entry: entry }, defaults: { content: '' } }
+        );
+        const created = data[1];
+        if (!created) return ezPromiseError("Duplicate Entry");
+        return this.readCard(card);
     }
 
-    readCard(card) {
-        return this.cards[card].findAll()
-            .then(rows => rows.map(r => [r.get('entry'), r.get('content')]));
+    async readCard(card) {
+        const rows = await this.cards[card].findAll();
+        return await rows.map(r => [r.get('entry'), r.get('content')]);
     }
 
-    updateContent(card, entry, text) {
-        return this.cards[card]
-            .find({ where: { entry: entry } })
-            .then(record => {
-                if (!record) return ezPromiseError('Entry Not Found');
-                return record;
-            }).then(record => record.updateAttributes({ content: text })
-                .then(() => this.readCard(card)));
+    async updateContent(card, entry, text) {
+        const record = await this.cards[card].find({ where: { entry: entry } });
+        if (!record) return ezPromiseError('Entry Not Found');
+        await record.updateAttributes({ content: text });
+        return await this.readCard(card);
     }
 
-    deleteCard(card) {
-        return this.Card
-            .destroy({ where: { name: card }, limit: 1 })
-            .then(() => this.cards[card].drop())
-            .then(() => delete this.cards[card])
-            .then(() => this.cList());
+    async deleteCard(card) {
+        await this.Card.destroy({ where: { name: card }, limit: 1 });
+        await this.cards[card].drop();
+        delete this.cards[card];
+        return this.cList();
     }
 
-    deleteEntry(card, entry) {
-        return this.cards[card]
-            .destroy({ where: { entry: entry }, limit: 1 })
-            .then(() => this.readCard(card));
+    async deleteEntry(card, entry) {
+        await this.cards[card].destroy({ where: { entry: entry }, limit: 1 });
+        return this.readCard(card);
     }
 }
